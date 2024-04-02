4b38e2c7397a8e1f20ccc4d62bb89aff1342a16e
pranay jain <pranayjain1382.com>
2024-04-02 15:45:54 +0530

    changes in the index.js

--- 05_Backend/index.js
+++ 05_Backend/index.js
@@ -1,31 +1,27 @@
 // External dependencies
 import express from "express";
-import "dotenv/config";
-import cors from "cors";
+import 'dotenv/config';
+import cors from 'cors';
 import { connectDatabase } from "./database.js";
-import authRoutes from "./router/auth.js";
-import orgRoutes from "./router/organization.js";
-import userRoutes from "./router/users.js";
-import adminRoutes from "./router/admin.js";
-
-// Connect to MongoDB
-connectDatabase();
+import authRoutes from './router/auth.js';
+import orgRoutes from './router/organization.js';
+import userRoutes from './router/users.js';
+import adminRoutes from './router/admin.js';
 
 // Middleware
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Internal dependencies
+// Connect to MongoDB and start server if successful, otherwise log error
+connectDatabase().then(() => {
+    // Routes
+    app.use('/', authRoutes);
+    app.use('/org', orgRoutes);
+    app.use('/donor', userRoutes);
+    app.use('/admin', adminRoutes);
 
-// RoutesS
-app.use("/", authRoutes);
-app.use("/org", orgRoutes);
-app.use("/donor", userRoutes);
-app.use("/admin", adminRoutes);
-
-
-    
+    console.log("hello");
     // Start server
     const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
@@ -34,5 +30,4 @@ app.use("/admin", adminRoutes);
 }).catch(error => {
     console.error("Error connecting to database:", error);
     process.exit(1); // Exit the process with error status code
-
 });
